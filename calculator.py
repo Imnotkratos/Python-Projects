274ada080921b5e1d3ea05115652536622a635e4
Stefano Saldarriaga <148514299+Imnotkratos@users.noreply.github.com>
2024-07-20 23:52:10 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -5,7 +5,7 @@ app.title("My first project")
 app.config(background="black")
 
 e = Entry(app,width=35,borderwidth=5,bg="black",fg="white")
-e.grid(row=0,column=0,columnspan=3,padx=20,pady=20)
+e.grid(row=0,column=0,columnspan=10,padx=20,pady=20)
 
 #Funciones de los botones
 def button_add(num):
@@ -21,7 +21,7 @@ def number1():
 
 
 def button_equal():
-
+    
     number2 = e.get()
     global snum
     snum = int(number2)
@@ -35,6 +35,8 @@ def button_equal():
         e.insert(0,num * snum)
     elif operation == "divide":
         e.insert(0,num / snum)
+    elif operation == "exp":
+        e.insert(0,num**snum)
 
 #Operation functions
 def button_plus():
@@ -61,6 +63,11 @@ def button_divide():
     number1()
     e.delete(0,END)
 
+def button_exp():
+    global operation
+    operation = "exp"
+    number1()
+    e.delete(0,END)
 
 
 def delete():
@@ -80,14 +87,15 @@ button9 = Button(app,fg="white",bg="#2B2422",text="9",padx=30,pady=30,command=la
 button0 = Button(app,fg="white",bg="#2B2422",text="0",padx=30,pady=30,command=lambda: button_add(0)).grid(row=4,column=0)
 
 #Result
-equal = Button(app,fg="white",bg="#2B2422",text="=",padx=30,pady=30,command=button_equal).grid(row=4,column=1)
+equal = Button(app,fg="white",bg="#A7A3A1",text="=",padx=30,pady=30,command=button_equal).grid(row=4,column=1)
 
 #Operations
-plus = Button(app,fg="white",bg="#2B2422",text="+",padx=30,pady=30,command=button_plus).grid(row=1,column=4)
-subtract = Button(app,fg="white",bg="#2B2422",text="-",padx=30,pady=30,command=button_rest).grid(row=2,column=4)
-multiply = Button(app,fg="white",bg="#2B2422",text="x",padx=30,pady=30,command=button_multiply).grid(row=3,column=4)
-divide = Button(app,fg="white",bg="#2B2422",text="/",padx=30,pady=30,command=button_divide).grid(row=4,column=4)
-clear = Button(app,fg="white",bg="#2B2422",text="C",padx=30,pady=30,command=delete).grid(row=4,column=2)
+plus = Button(app,fg="white",bg="#CD6B20",text="+",padx=30,pady=30,command=button_plus).grid(row=1,column=4)
+subtract = Button(app,fg="white",bg="#CD6B20",text="-",padx=30,pady=30,command=button_rest).grid(row=2,column=4)
+multiply = Button(app,fg="white",bg="#CD6B20",text="x",padx=30,pady=30,command=button_multiply).grid(row=3,column=4)
+divide = Button(app,fg="white",bg="#CD6B20",text="/",padx=30,pady=30,command=button_divide).grid(row=4,column=4)
+exp = Button(app,fg="white",bg="#CD6B20",text="^",padx=30,pady=30,command=button_exp).grid(row=5,column=4)
+clear = Button(app,fg="white",bg="#A7A3A1",foreground="white",text="C",padx=30,pady=30,command=delete).grid(row=4,column=2)
 
 
 app.mainloop()
